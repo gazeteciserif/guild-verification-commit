1606f5739452a21a520234c55c2935c651bdf81f
gazeteciserif <38495932+gazeteciserif@users.noreply.github.com>
2026-03-06 00:26:28 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:24:53.698Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T21:26:22.811Z
+Commit: Fix CSS styling bug on mobile devices
