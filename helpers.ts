f60b87396caf6eee938c379bd51801915830677e
gazeteciserif <38495932+gazeteciserif@users.noreply.github.com>
2026-03-06 00:27:58 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:27:31.068Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T21:27:52.306Z
+Commit: Patch API endpoint error handling
