0e13f8b7fb8607d1b7c75f3748123ab221e3b036
gazeteciserif <38495932+gazeteciserif@users.noreply.github.com>
2026-03-06 00:25:07 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:24:55.941Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T21:25:02.012Z
+Commit: Fix memory leak in event handlers
