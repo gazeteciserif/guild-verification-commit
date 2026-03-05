5b6badc11df96e0fe2f14f4c47e4e3be7761a0a6
gazeteciserif <38495932+gazeteciserif@users.noreply.github.com>
2026-03-06 00:25:01 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:24:50.275Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T21:24:55.941Z
+Commit: Fix broken links in documentation
